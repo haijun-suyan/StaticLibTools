0537f7a1919021fb1075ec5979631a0b193b01a4
yanhaijun <15261882407@163.com>
2023-10-17 16:08:39 +0800

    .a静态库项目包

---
+++ StaticLibTools/StaticLibTools/StaticLibTools.h
@@ -0,0 +1,12 @@
+//
+//  StaticLibTools.h
+//  StaticLibTools
+//
+//  Created by haijunyan on 2023/10/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibTools : NSObject
+
+@end
---
+++ StaticLibTools/StaticLibTools/StaticLibTools.m
@@ -0,0 +1,12 @@
+//
+//  StaticLibTools.m
+//  StaticLibTools
+//
+//  Created by haijunyan on 2023/10/17.
+//
+
+#import "StaticLibTools.h"
+
+@implementation StaticLibTools
+
+@end
